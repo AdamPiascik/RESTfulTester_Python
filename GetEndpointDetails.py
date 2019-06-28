6f1c20a29a3e5730b566e89ca9749df06a4e0c3c
Adam Piascik <adam.piascik@avamae.co.uk>
2019-06-28 09:51:51 +0100

    finished functionality to include JWT access token in output csv

--- GetEndpointDetails.py
+++ GetEndpointDetails.py
@@ -5,7 +5,7 @@ import os
 
 # ----------------------------------------------------------------------
 
-def main():
+def ParseArgs():
     parser = argparse.ArgumentParser(description="Tool for generating CSV\
                      files containing the full set of information needed to\
                      test all endpoints of a RESTful API.",
@@ -21,12 +21,12 @@ def main():
                         action="append",
                         dest="auth",
                         type=str)
-    parser.add_argument("-p", "--parameters",
+    parser.add_argument("-p", "--parametersFile",
                         metavar="",
                         help="Specifies the JSON file containing dummy\
                         data. This data will be use to populate the required\
                         parameters for relevant endpoints.",
-                        dest="parameters",
+                        dest="parametersFile",
                         type=str)
     parser.add_argument("-v", "--version",
                         metavar="",
@@ -37,59 +37,89 @@ def main():
                         default=1,)
     try:
         args = parser.parse_args()
+        return args
     except:
         return
 
 # ----------------------------------------------------------------------
-    sanitised_url = args.url.replace("https://", "").replace("/", "")
-    version = "-v" + str(args.version)
 
-    with requests.get(args.url + "/v" + str(args.version) + "/doc.json") as url:
+def GetParametersForEndpoint(requestedEndpoint, parameters_file):
+    if parameters_file is not None:
+        with open(parameters_file, 'r') as jsonData:
+            endpoints = json.load(jsonData)
+            for endpoint, parameters in endpoints.items():
+                if endpoint == requestedEndpoint:
+                    return parameters
+
+def ParseParameters(requestedEndpoint, parameters_file):
+    dict = GetParametersForEndpoint(requestedEndpoint, parameters_file)
+    params = ""
+    if dict is not None:
+        for key, value in dict.items():
+            params += (key + "=" + str(value) + ";")
+        return params[:-1]
+    return params
+
+def GetAllAccessTokens(baseurl, auth_endpoints, parameters_file):
+    access_tokens = {}
+    for auth in auth_endpoints:
+        with requests.post(baseurl + auth, json=
+        GetParametersForEndpoint(auth, parameters_file))as response:
+            try:
+                access_tokens[auth] = response.json()["access_token"]
+            except:
+                print("Authorization failed for" + auth)
+    return access_tokens
+
+def GetAccessTokenForEndpoint(endpoint_description, access_tokens):
+    identifier = "auth="
+    start = endpoint_description.find(identifier)
+    if start != -1:
+        if endpoint_description.find(" ", start) != -1:
+            end = endpoint_description.find(" ", start)
+        else:
+            end = len(endpoint_description)
+        auth_endpoint = endpoint_description[start + len(identifier) : end]
+        try:
+            return access_tokens[auth_endpoint]
+        except:
+            return ""
+    return ""
+
+# ----------------------------------------------------------------------
+
+def main():
+    args = ParseArgs()
+    baseurl = args.url
+    auth_endpoints = args.auth
+    parameters_file = args.parametersFile
+    version = str(args.version)
+
+    sanitised_url = baseurl.replace("https://", "").replace("http://", "").replace("/", "")
+
+    with requests.get(baseurl + "/v" + version + "/doc.json") as url:
         doc_file = url.json()
 
-    def GetParametersForEndpoint(requestedEndpoint):
-        if args.parameters is not None:
-            with open(args.parameters, 'r') as jsonData:
-                endpoints = json.load(jsonData)
-                for endpoint, parameters in endpoints.items():
-                    if endpoint == requestedEndpoint:
-                        return parameters
+    if auth_endpoints:
+        access_tokens = GetAllAccessTokens(baseurl, auth_endpoints, parameters_file)
 
-    def ParseParameters(requestedEndpoint):
-        dict = GetParametersForEndpoint(requestedEndpoint)
-        params = ""
-        if dict is not None:
-            for key, value in dict.items():
-                params += (key + "=" + str(value) + ";")
-            return params[:-1]
-        return params
+    endpoint_details = []
+    for endpoint, methods in doc_file["paths"].items():
+        for method, data in methods.items():
+            try:
+                token = GetAccessTokenForEndpoint(data["description"], access_tokens)
+            except:
+                token = ""
+            line = ",".join([endpoint, method, token, ParseParameters(endpoint, parameters_file)])
+            endpoint_details.append(line)
 
-    access_tokens = {}
-    def GetAccessTokens(auth_endpoints):
-        for auth in auth_endpoints:
-            with requests.post(args.url + auth, json=GetParametersForEndpoint(auth)) as response:
-                try:
-                    access_tokens[auth] = response.json()["access_token"]
-                except:
-                    print("Authorization failed for" + auth)
+    filename = os.path.join("."
+                            ,sanitised_url
+                            +version + ".csv")
 
-    GetAccessTokens(args.auth)
-
-    print(access_tokens)
-
-    # list_endpoints = []
-    # for endpoint, methods in doc_file["paths"].items():
-    #     for method, data in methods.items():
-    #         line = ",".join([endpoint, method, ParseParameters(endpoint)])
-    #         list_endpoints.append(line)
-    #
-    # filename = os.path.join("."
-    #                         ,sanitised_url
-    #                         +version + ".csv")
-    #
-    # with open(filename, "w") as file:
-    #     for item in list_endpoints:
-    #         file.write(item + "\n")
+    with open(filename, "w") as file:
+        for item in endpoint_details:
+            file.write(item + "\n")
 
 # ----------------------------------------------------------------------
 
